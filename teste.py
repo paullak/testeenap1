ca6cc57f55b75529f05064d8238202c4f299d9e9
paullak <69565868+paullak@users.noreply.github.com>
2024-11-13 11:45:37 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
 
-st.select_slider("Grau de Satisfação", [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+numero = st.select_slider("Grau de Satisfação", [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+
+
+
+st.text('Seu número é ' + str(numero))
 
